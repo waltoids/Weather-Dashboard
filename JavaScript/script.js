07554c9cff8811c84bda5d6e36f6860fe7dc5fee
Walter Joy IV <waltoids@users.noreply.github.com>
2020-05-29 19:01:18 -0400

    global  variables initalized

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+const searchBarEl = $("#search-input");
+const searchBtn = $(".btn");
+const cityHistory = $("#city-history");
+const weatherCurretn = $("#current-weather");
+const weatherForecast = $("#weather-forecast");
+const forecastRow = $("#forecast-row");
